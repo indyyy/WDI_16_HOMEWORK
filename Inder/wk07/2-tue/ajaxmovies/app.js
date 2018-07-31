eee4a41b1cd71cdfa4447bd05345a81ec4976cc0
Inder <inderyyz@gmail.com>
2018-08-01 08:55:59 +1000

    asaxmovies homework

---
+++ Inder/wk07/2-tue/ajaxmovies/app.js
@@ -0,0 +1,43 @@
+// defining the callback function that will happen
+// if the request succeeds.
+function onSuccess(responseData) {
+  console.log(responseData)
+  responseData.Search.forEach(function(movie)
+  {console.log(movie.Title)
+
+    $('.movielist').append($('<p>').text(movie.Title));
+
+
+  });
+  
+  //console.log("test");
+  // celebrate!
+};
+
+
+//var moviename = $('input').val();
+//  console.log(moviename);
+
+document.querySelector("input").addEventListener("change",function(){
+  
+  var moviesearch=document.querySelector("input").value
+  console.log(moviesearch)
+
+
+
+$.ajax({
+// What kind of request
+method: 'GET',
+
+// The URL for the request
+url: "http://omdbapi.com/?s="+moviesearch+"&apikey=2f6435d9",
+//http://omdbapi.com/?t=#{movieinput}&apikey=2f6435d9")
+
+// The type of data we want back
+dataType: 'json',
+
+// Code to run if the request succeeds; the JSON
+// response is passed to the function as an argument.
+}).done(onSuccess);
+
+})
