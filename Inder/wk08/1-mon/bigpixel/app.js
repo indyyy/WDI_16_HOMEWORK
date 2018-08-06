6b67db645ae31b7673ecb281e767e51d4f385de9
Inder <inderyyz@gmail.com>
2018-08-06 20:19:28 +1000

    Second Commit

--- Inder/wk08/1-mon/bigpixel/app.js
+++ Inder/wk08/1-mon/bigpixel/app.js
@@ -6,3 +6,7 @@ $( "#selectcolor" ).click(function() {
   $('.brush').attr('style','background:'+userInput);
   event.preventDefault();
 });
+
+for (i = 0; i < 1000; i++) {
+  $( "body" ).append( "<div class='square'></div>" );
+}
