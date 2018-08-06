382165ec40c6ea755e852ef4fc7d0a1277c6f84d
Inder <inderyyz@gmail.com>
2018-08-07 09:00:53 +1000

    commit with spinner

--- Inder/wk08/1-mon/bigpixel/app.js
+++ Inder/wk08/1-mon/bigpixel/app.js
@@ -1,5 +1,13 @@
 
 
+$( "#resetColor" ).click(function() {
+  $('.square').attr('style','background: white');
+});
+
+$( "#resetMovie" ).click(function() {
+  $('.square').attr('style','background: white');
+});
+
 $( "#selectcolor" ).click(function() {
   var userInput = $('#inputcolor').val().trim();
   console.log(userInput)
@@ -7,8 +15,16 @@ $( "#selectcolor" ).click(function() {
   event.preventDefault();
 });
 
+
+var theInput = document.getElementById("favcolor");
+   theInput.addEventListener("input", function() {
+   console.log(theInput.value)
+   $('.brush').attr('style','background:'+theInput.value);
+
+    }, false);
+
 for (i = 0; i < 1000; i++) {
-  $( "body" ).append( "<div class='square'></div>" );
+  $( ".pixelwrapper" ).append( "<div class='square'></div>" );
 }
 
 
@@ -19,3 +35,20 @@ $('.square').on('click', function(){
 });
 
 
+function onSuccess(responseData) {
+  console.log(responseData)
+  console.log(responseData.Search[0].Poster)
+  $(".pixelwrapper").attr('style','background-image:'+'url('+responseData.Search[0].Poster+')')
+  
+};
+
+$('#selectmovie').click(function() {
+  var moviesearch = $('#inputmovie').val().trim();
+  event.preventDefault();
+  console.log(moviesearch)
+  $.ajax({
+    method: 'GET',
+    url: "http://omdbapi.com/?s="+moviesearch+"&apikey=2f6435d9",
+    dataType: 'json',
+    }).done(onSuccess); 
+})
